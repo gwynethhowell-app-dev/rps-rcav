1abb61f7e27dd1a3c703aa44a48a44e1fc0eb95f
gwynethhowell <ghowell@uchicago.edu>
2023-01-25 23:54:56 +0000

    rock page w random rock/paper/scissors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
+
 class ApplicationController < ActionController::Base
   layout(false)
 
   # Add your actions below this line
   # ================================
+
+    def play_rock
+      render({ :template => "game_templates/user_rock.html.erb" })
+    end
 
 end
