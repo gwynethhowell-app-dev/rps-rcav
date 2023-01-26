8f46839161d8a24b8c2741ad2750e84a83dab3b3
gwynethhowell <ghowell@uchicago.edu>
2023-01-26 00:44:32 +0000

    rock and paper pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,27 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
-    def play_rock
-      render({ :template => "game_templates/user_rock.html.erb" })
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+  end
+
+  def play_rock
+    render({ :template => "game_templates/user_rock.html.erb" })
+  end
+
+  def play_paper
+  @comp_move = ["rock", "paper", "scissors"].sample
+
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    else @comp_move == "scissors"
+      @outcome = "lost"
     end
+
+    render({ :template => "game_templates/user_paper.html.erb"})
+  end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  self.get("/rock", { :controller => "application", :action => "play_rock" })
+  self.get("/", { :controller => "application", :action => "homepage" })
 
+  get("/rock", { :controller => "application", :action => "play_rock" })
 
+  get("/paper", { :controller => "application", :action => "play_paper"})
 end
